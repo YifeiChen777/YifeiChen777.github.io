016cff96262e63e5f915004e79110e491445599c
Caroline Chen <yc4ag@virginia.edu>
2021-01-14 14:55:08 -0500

    first commit

---
+++ a0_template/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+	createCanvas(800,600); // make an HTML canvas element width x height pixels
+}
+
+function draw() {
+	background(225);
+	textSize(32);
+	// fill(180);
+	stroke('green');
+	for (let i = 0; i < hour(); i++){
+		noFill();
+		circle(300, 160, i * 7);
+	}
+
+	for (let i = 0; i < minute()/2; i++){
+		noFill();
+		stroke('blue');
+		circle(600, 300, i * 6);
+	}
+	stroke('red');
+	for (let i = 0; i < second()/2; i++){
+		noFill();
+		circle(190, 410, i * 6);
+	}
+}
