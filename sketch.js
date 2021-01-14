55996c28765d1004a51171d2a3450b44527863a8
Caroline Chen <yc4ag@virginia.edu>
2021-01-14 15:49:57 -0500

    seec color

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+const { FILL } = require("./p5");
+
 function setup() {
 	createCanvas(800,600); // make an HTML canvas element width x height pixels
 }
@@ -19,7 +21,10 @@ function draw() {
 	}
 	stroke('red');
 	for (let i = 0; i < second()/2; i++){
-		noFill();
-		circle(190, 410, i * 6);
+		// noFill();
+		secColor = color('red');
+		secColor.setAlpha(128 + 128 * sin(PI * millis() / 1000));
+		fill(secColor);
+		circle(190, 410, i * 10);
 	}
 }
